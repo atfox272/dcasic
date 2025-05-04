2236f66a31d93def1d4f37a57a9f4373830ea42a
atfox272 <trananhtai577@gmail.com>
2025-05-04 21:21:17 +0700

    mod: sim script & sim time

--- tb/dcasic_tb.sv
+++ tb/dcasic_tb.sv
@@ -10,7 +10,7 @@
 `define RST_DLY_START   3
 `define RST_DUR         9
 
-`define END_TIME        370000000
+`define END_TIME        500000000
 
 // DVP Physical characteristic
 // -- t_PDV = 5 ns = (5/INTERNAL_CLK_PERIOD)*DUT_CLK_PERIOD = (5/8)*2
